35796f623065b86d71f9e539f068523b4a706df3
Daniel Ostrovsky <danduh@gmail.com>
2025-10-13 11:49:27 +0300

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/nE6rJqWltW

---
+++ apps/openai-apps-sdk-nx/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@openai-apps-sdk-nx/openai-apps-sdk-nx" />
+    </div>
+  );
+}
+
+export default App;
